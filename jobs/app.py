3649cc3534aefc85378b01746d946f9c5190cab7
siliconfish <siliconfish@yahoo.com>
2020-02-28 20:38:32 -0500

    Update app.py

--- jobs/app.py
+++ jobs/app.py
@@ -17,7 +17,7 @@ def open_connection():
     
     return connection
 
-def execute_sql(sql, values = (), commit = False, single = False):
+def execute_sql(sql, values=(), commit=False, single=False):
     connection = open_connection()
     cursor = connection.execute(sql, values)
 
