11473050eb83ccb372f4ab21099545c639ac936b
siliconfish <siliconfish@yahoo.com>
2020-02-29 21:13:23 -0500

    Update app.py

--- jobs/app.py
+++ jobs/app.py
@@ -62,7 +62,7 @@ def review(employer_id):
         rating = request.form['rating']
         title = request.form['title']
         status = request.form['status']
-        date = datetime().now().strftime("%m/%d/%y")
+        date = datetime.datetime.now().strftime("%m/%d/%y")
 
         execute_sql('INSERT INTO review (review, rating, title, date, status, employer_id) VALUES (?, ?, ?, ?, ?, ?)', (review, rating, title, date, status, employer_id), commit=True)
 
